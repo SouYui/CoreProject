92f589fc216bb1b3d9a8f149b55381a443cdb7d5
Sou Yui <luiscuriel911@aragon.unam.mx>
2022-01-25 21:17:21 -0600

    Fixes

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Administrators;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,8 @@ namespace WebAPI
             services.AddDbContext<BellotaContext>( opt => {
                 opt.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
             } );
+
+            services.AddMediatR(typeof(GetAdministrador.Manejador).Assembly);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
